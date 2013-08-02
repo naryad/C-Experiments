9a89e9a712ace87fa9312ca79faf431020b17084
Narendra Yadala <narendra.yadala@gmail.com>
2013-08-02 13:22:19 +0530

    Initial Commit

---
+++ C Experiments/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C Experiments
+//
+//  Created by Narendra Yadala on 02/08/13.
+//  Copyright (c) 2013 Narendra Yadala. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
