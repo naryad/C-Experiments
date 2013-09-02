c2d885d594d1bfcc8ca524f5fc130ffe590c0a1f
Narendra Yadala <narendra.yadala@gmail.com>
2013-09-02 20:58:51 +0530

    C Experiments Initial Commit

--- C Experiments/main.c
+++ C Experiments/main.c
@@ -5,14 +5,29 @@
 //  Created by Narendra Yadala on 02/08/13.
 //  Copyright (c) 2013 Narendra Yadala. All rights reserved.
 //
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[])
+int main()
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int x;            /* A normal integer*/
+    int *p;           /* A pointer to an integer ("*p" is an integer, so p
+                       must be a pointer to an integer) */
+    
+    p = &x;           /* Read it, "assign the address of x to p" */
+    scanf( "%d", &x );          /* Put a value in x, we could also use p here */
+    
+    printf( "*p = %d\n", *p ); /* Note the use of the * to get the value */
+    printf("*&x = %d\n", *&x);
+    printf("x = %d\n", x);
+    
+    printf( "p = %p\n", p);
+    printf( "&x = %p\n", &x);
+    
+    printf( "sizeof(*p) = %lu\n", sizeof(*p));
+    printf( "sizeof(x) = %lu\n", sizeof(x));
+    
+    printf( "&p = %p\n", &p);
+    
+    getchar();
 }
-
